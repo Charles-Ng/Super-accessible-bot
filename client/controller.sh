102fddb6da6d5c35aab7755026a0aeb4e74426f5
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-01-08 04:14:22 -0500

    Added all the client side support, it should be working mostly now

---
+++ client/controller.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# hi="cd .."
+# $hi
+# ls
+
+
+if [ "$#" -eq 2 ]; then
+    printf "" 1> $2
+
+    while read line; do
+        if [ ${line:0:3} == "ls" ]; then
+            $line 1>> $2
+        else 
+            $line
+            if [ "$?" == 0 ] ; then
+                echo "Command executed." 1>> $2 
+            else
+                echo "Command could not be executed." 1>> $2 
+            fi
+        fi
+        echo "--------------------" 1>> $2
+    done <$1
+else
+    echo "Incorrect Usage"
+    exit 1
+fi
